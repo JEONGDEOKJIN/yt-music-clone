e4b6823b8b6048cf12f7c1c0a0331e7b47ff0aaa
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2024-04-25 21:11:41 +0900

    0425 수정

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,7 +11,7 @@ const page = async () => {
   // console.log("1️⃣ before sleep")
 
   // // '서버에 갔다 오는 시간' 을 이렇게 테스트 해볼 수도? | 비동기 처리를 이렇게 해볼수도?
-  // await sleep(5000) // 2초 대기(비동기 처리) 후 동기 처리(아래 코드 실행)
+  // await sleep(5000) // 2초 대기(비동기 처리) 후 동기 처리(아래 코드 실행) | 오캐이 테스트
 
   console.log("2️⃣ after sleep");
   // throw new Error("my error")
